e41c1de97847b92dd2ea003dbab969a2ebb5ba5d
Snehal Roy <snehal.roy62@gmail.com>
2026-03-02 14:33:23 -0500

    Update .gitignore to include Gradle build files and IDE settings

--- go/go.mod
+++
@@ -1,3 +0,0 @@
-module challenge
-
-go 1.25
--- kotlin/gradle/src/main/kotlin/client/Problem.kt
+++
@@ -1,7 +0,0 @@
-package com.css.challenge.client
-
-/** Problem represents a test problem */
-data class Problem(
-    val testId: String,
-    val orders: List<Order>
-)
